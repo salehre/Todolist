7c0954bf94d732967c503f39b30371964e892cc7
salehre <salehrezaeipoor123@gmail.com>
2026-08-21 00:41:22 +0330

    24.9

--- components/dashboard/DashboardHeader.vue
+++ components/dashboard/DashboardHeader.vue
@@ -114,7 +114,7 @@
             @click="toggleDark"
             class="flex h-9 w-9 items-center justify-center rounded-xl text-slate-400 transition-colors hover:bg-slate-100 hover:text-slate-600"
         >
-          <Icon :icon="isDark ? 'mdi:weather-sunny' : 'mdi:weather-night'" class="text-lg" />
+          <MorphIcon :icon="isDark ? heroMoon : heroSun" spring="smooth" class="text-lg" :size="18" />
         </button>
       </div>
 
@@ -236,6 +236,10 @@ import { useTheme } from '@/composables/useTheme'
 import { useLocale } from '@/composables/useLocale'
 import { useAuth } from '@/composables/useAuth'
 import { useNotifications } from '@/composables/useNotifications'
+import { MorphIcon } from 'morphicons/vue'
+
+const heroSun = "M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+const heroMoon = "M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
 
 const route = useRoute()
 const router = useRouter()
